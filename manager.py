445ccb84fba42f98ca3356d9f90ffd09ebeb2971
LitdeozZtan <wang4358103@163.com>
2021-05-11 21:59:46 +0800

    数据库初始化，增加配置类

--- manager.py
+++ manager.py
@@ -2,8 +2,17 @@
 # Author: json_steve
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# 数据库初始化
+db = SQLAlchemy(app)
+
+
+# 配置类
+class Config(object):
+    DEBUG = True
+app.config.from_object(Config)
 
 
 @app.route('/')
@@ -13,3 +22,5 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+
+
