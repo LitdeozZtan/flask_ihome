4240d1919c96d7784b0525d6fad195fda059cfa9
LitdeozZtan <wang4358103@163.com>
2021-05-16 16:26:07 +0800

    redis导入初始化

--- manager.py
+++ manager.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
 
 app = Flask(__name__)
 # 数据库初始化
@@ -11,8 +12,14 @@ db = SQLAlchemy(app)
 
 # 配置类
 class Config(object):
+    """配置类信息"""
     DEBUG = True
+    # redis链接配置,配置信息在这里，被自己调用
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 app.config.from_object(Config)
+# 初始化redis
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=0)
 
 
 @app.route('/')
