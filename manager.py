a6b7cd4f9dd3cdc091ad02ed7cf9fca42cceba58
LitdeozZtan <wang4358103@163.com>
2021-05-29 18:20:22 +0800

    csrf初始化

--- manager.py
+++ manager.py
@@ -1,9 +1,10 @@
 # -*- coding:utf-8 -*- 
 # Author: json_steve
 
-from flask import Flask
+from flask import Flask,current_app
 from flask_sqlalchemy import SQLAlchemy
 import redis
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
@@ -11,6 +12,8 @@ app = Flask(__name__)
 # 配置类
 class Config(object):
     """配置类信息"""
+    #SECRET_KET = 'OZiDpgU1ir/pmSs8nFoqs6Z63RcupX58VhmiYQ0/DQDTiyXs1+duo6EY29SI1Xe4'
+    SECRET_KEY = "bxOaxOKPaeZaipSvq7rjfeYtYvG5jPvwIgGZsteuTGhTSrKAHh/Q6eGHQm2Yw671"
     DEBUG = True
     # redis链接配置,配置信息在这里，被自己调用
     REDIS_HOST = '127.0.0.1'
@@ -25,9 +28,11 @@ app.config.from_object(Config)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=0)
 # 数据库初始化 顺序很重要，在config之后
 db = SQLAlchemy(app)
+# csrf初始化
+csrf = CSRFProtect(app)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", 'POST'])
 def index():
     redis_store.set(name='xiaoli', value='32')
     return 'index'
